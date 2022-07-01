4b1152c5c434a896a638da74da45c1cb7ad8d766
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-07-01 09:30:07 +0300

    refactor with hooks

--- src/componants/calculetor.js
+++ src/componants/calculetor.js
@@ -1,106 +1,98 @@
-import React, { Component } from 'react';
-import './calculetor.css';
+import React, { useState } from 'react';
+import './calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      total: 0,
-      next: '',
-      operation: '',
-    };
-  }
-
-  eventHandler = (e) => {
+function Calculator() {
+  const [data, setData] = useState({
+    obj: { total: 0, next: '', operation: '' },
+  });
+  const eventHandler = (e) => {
     const val = e.target.textContent;
-    this.setState((obj) => calculate(obj, val));
+    setData(({ obj }) => ({ obj: calculate(obj, val) }));
   };
-
-  render() {
-    const { total, next, operation } = this.state;
-    const results = `
+  const results = `
     ${
-  total || operation || next
-    ? `${total || ''} ${operation || ''} ${next || ''}`
+  data.obj.total || data.obj.operation || data.obj.next
+    ? `${data.obj.total || ''} ${data.obj.operation || ''} ${
+      data.obj.next || ''
+    }`
     : 0
 }`;
-    return (
-      <div className="calculator">
-        <div className="output__section">
-          <div className="current">{results}</div>
-        </div>
+  return (
+    <div className="calculator">
+      <div className="output__section">
+        <div className="current">{results}</div>
+      </div>
 
-        <div className="input__section">
-          <div className="output-ul">
-            <button
-              type="button"
-              onClick={this.eventHandler}
-              onKeyDown={this.eventHandler}
-              id="clear"
-              className="key"
-            >
-              Ac
-            </button>
-            <button type="button" onClick={this.eventHandler} id="plus-minus" className="key">
-              +/-
-            </button>
-            <button type="button" onClick={this.eventHandler} id="percent" className="key">
-              %
-            </button>
-            <button type="button" onClick={this.eventHandler} id="divide" className="key action">
-              ÷
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key">
-              7
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key">
-              8
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key">
-              9
-            </button>
-            <button type="button" onClick={this.eventHandler} id="times" className="key action">
-              x
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key">
-              4
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key">
-              5
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key">
-              6
-            </button>
-            <button type="button" onClick={this.eventHandler} id="minus" className="key action">
-              -
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key">
-              1
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key">
-              2
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key">
-              3
-            </button>
-            <button type="button" onClick={this.eventHandler} id="plus" className="key action">
-              +
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key zero">
-              0
-            </button>
-            <button type="button" onClick={this.eventHandler} className="key decimal">
-              .
-            </button>
-            <button type="button" onClick={this.eventHandler} id="equals" className="key action">
-              =
-            </button>
-          </div>
+      <div className="input__section">
+        <div className="output-ul">
+          <button
+            type="button"
+            onClick={eventHandler}
+            onKeyDown={eventHandler}
+            id="clear"
+            className="key"
+          >
+            Ac
+          </button>
+          <button type="button" onClick={eventHandler} id="plus-minus" className="key">
+            +/-
+          </button>
+          <button type="button" onClick={eventHandler} id="percent" className="key">
+            %
+          </button>
+          <button type="button" onClick={eventHandler} id="divide" className="key action">
+            ÷
+          </button>
+          <button type="button" onClick={eventHandler} className="key">
+            7
+          </button>
+          <button type="button" onClick={eventHandler} className="key">
+            8
+          </button>
+          <button type="button" onClick={eventHandler} className="key">
+            9
+          </button>
+          <button type="button" onClick={eventHandler} id="times" className="key action">
+            x
+          </button>
+          <button type="button" onClick={eventHandler} className="key">
+            4
+          </button>
+          <button type="button" onClick={eventHandler} className="key">
+            5
+          </button>
+          <button type="button" onClick={eventHandler} className="key">
+            6
+          </button>
+          <button type="button" onClick={eventHandler} id="minus" className="key action">
+            -
+          </button>
+          <button type="button" onClick={eventHandler} className="key">
+            1
+          </button>
+          <button type="button" onClick={eventHandler} className="key">
+            2
+          </button>
+          <button type="button" onClick={eventHandler} className="key">
+            3
+          </button>
+          <button type="button" onClick={eventHandler} id="plus" className="key action">
+            +
+          </button>
+          <button type="button" onClick={eventHandler} className="key zero">
+            0
+          </button>
+          <button type="button" onClick={eventHandler} className="key decimal">
+            .
+          </button>
+          <button type="button" onClick={eventHandler} id="equals" className="key action">
+            =
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default Calculator;
